b6970311cecf9d9da03dd2aed6393255f8de2cf2
Enabwodahs1997 <sesolstice1997@gmail.com>
2026-08-30 16:32:15 -0600

    Login page wasn't redirecting to the home page after successful login. Added a line to navigate to the home page after successful login.

--- src/app/guards/login.guard.ts
+++ src/app/guards/login.guard.ts
@@ -1,16 +1,16 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router, UrlTree } from '@angular/router';
-import { first, map } from 'rxjs/operators';
 import { AuthService } from '../services/auth.service';
 
 @Injectable({ providedIn: 'root' })
 export class LoginGuard implements CanActivate {
   constructor(private auth: AuthService, private router: Router) {}
 
-  canActivate(): any {
-    return this.auth.user$.pipe(
-      first(),
-      map(user => (user ? this.router.createUrlTree(['/']) : true))
-    );
+  canActivate(): boolean | UrlTree {
+    if (!this.auth.currentUser) {
+      return true;
+    }
+
+    return this.router.createUrlTree(['/']);
   }
 }
